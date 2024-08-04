be19f870e61d557acd66e01e2c6d7d947349597b
Yaozhen-yi <a021560@gmail.com>
2024-08-04 20:27:27 +0800

    修改vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/bee-yard-vue/', // 这里添加你的仓库名称
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: '/bee-yard-vue/' // 这里添加你的仓库名称
+  }
 })
