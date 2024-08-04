f2fbe2588d213b52776f792545eec72fccfdebf0
Yaozhen-yi <a021560@gmail.com>
2024-08-04 23:54:26 +0800

    修改表單

--- src/components/webform.vue
+++ src/components/webform.vue
@@ -1,31 +1,43 @@
 <script setup>
 import { ref } from 'vue';
 import useVuelidate from '@vuelidate/core';
-import { required, email, numeric } from '@vuelidate/validators';
+import { required, email, numeric, helpers } from '@vuelidate/validators';
 import { useRouter } from 'vue-router';
-
 
 
 const name = ref('');
 const phone = ref('');
 const address = ref('');
 const emailField = ref('');
-
+//綁定驗證規則
 const rules = {
-  name: { required },
-  phone: { required, numeric },
-  address: { required },
-  emailField: { required, email }
+  name: {
+    required: helpers.withMessage('姓名必須填寫', required)
+  },
+  phone: {
+    required: helpers.withMessage('電話必須填寫', required),
+    numeric: helpers.withMessage('電話號碼必須是數字', numeric)
+  },
+  address: {
+    required: helpers.withMessage('地址必須填寫', required)
+  },
+  emailField: {
+    required: helpers.withMessage('email必須填寫', required),
+    email: helpers.withMessage('不是一個有效的 e-mail 地址', email)
+  }
 };
-
+//使用Vuelidate進行驗證
 const $v = useVuelidate(rules, { name, phone, address, emailField });
+
 // /* 跳轉路由*/
 const router = useRouter();
 
 const submit = () => {
+  //觸發驗證
   $v.value.$touch();
+  //如果表單有錯誤,alert提示並返回
   if ($v.value.$invalid) {
-    alert('表單有錯誤,請確認輸入內容。');
+    alert('表单有错误，请检查输入内容。');
     return;
   }
 
@@ -41,42 +53,46 @@ const submit = () => {
   /*將data數據儲存到sessionStorage,在formDisplay中顯示 */
   sessionStorage.setItem('peopleData', JSON.stringify(data));
   //跳轉到formDisplay頁面
-  router.push('/formDisplay');
+  router.push('/displayForm');
   alert('表单提交成功！');
 };
-
-  
-
 </script>
 
 <template>
-  <div class="form">
+   <div class="form">
     <h3 class="title">訂購人資訊</h3>
-    <form id="dataForm"  @submit.prevent="submit"  method="post">
+    <form id="dataForm" @submit.prevent="submit" method="post">
       <div>
         <label for="name">姓名:</label>
-        <input id="name" v-model="name" type="text">
-        <span v-if="!$v.name.required && $v.name.$error">姓名必須填寫</span>
+        <input id="name" v-model="$v.name.$model" type="text">
+        <!--如果驗證錯誤,顯示錯誤訊息-->
+        <span v-show="$v.name.$error">
+          <span v-for="(error, index) in $v.name.$errors" :key="index">{{ error.$message }}</span>
+        </span>
       </div>
       <div>
         <label for="phone">電話:</label>
-        <input id="phone" v-model="phone" type="text">
-        <span v-if="!$v.phone.required && $v.phone.$error">請輸入電話號碼</span>
-        <span v-if="!$v.phone.numeric && $v.phone.$error">電號號碼必須是數字</span>
+        <input id="phone" v-model="$v.phone.$model" type="text">
+        <span v-show="$v.phone.$error">
+          <span v-for="(error, index) in $v.phone.$errors" :key="index">{{ error.$message }}</span>
+        </span>
       </div>
       <div>
         <label for="address">地址:</label>
-        <input id="address" v-model="address" type="text">
-        <span v-if="!$v.address.required && $v.address.$error">請輸入地址</span>
+        <input id="address" v-model="$v.address.$model" type="text">
+        <span v-show="$v.address.$error">
+          <span v-for="(error, index) in $v.address.$errors" :key="index">{{ error.$message }}</span>
+        </span>
       </div>
       <div>
         <label for="email">E-mail:</label>
-        <input id="email" v-model="emailField" type="email">
-        <span v-if="!$v.emailField.required && $v.emailField.$error">E-mail</span>
-        <span v-if="!$v.emailField.email && $v.emailField.$error">不是一個有效的 e-mail 地址</span>
+        <input id="email" v-model="$v.emailField.$model" type="email">
+        <span v-show="$v.emailField.$error">
+          <span v-for="(error, index) in $v.emailField.$errors" :key="index">{{ error.$message }}</span>
+        </span>
       </div>
       <button type="submit" class="btn">下一步</button>
-   </form>
+    </form>
   </div>
    
 </template>
